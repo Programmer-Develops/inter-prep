c8d927000cf9b6e35d70b95594f487de5af89254
Programmer-Develops <shachipandya29@gmail.com>
2025-04-07 14:25:38 +0000

    Apr 7, 2025, 7:55 PM

--- firebase/client.ts
+++ firebase/client.ts
@@ -19,7 +19,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 // const analytics = getAnalytics(app);
 
 export const auth = getAuth(app);
