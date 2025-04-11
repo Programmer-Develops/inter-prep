fbd974377f6a480fb25cf390b233e0d14ccac1f7
Programmer-Develops <shachipandya29@gmail.com>
2025-04-11 14:11:22 +0000

    Apr 11, 2025, 7:41 PM

---
+++ app/(root)/career/page.tsx
@@ -0,0 +1,9 @@
+import {Button} from "@/components/ui/button";
+import {getCurrentUser} from "@/lib/actions/auth.action";
+import {getInterviewsByUserId} from "@/lib/actions/general.action";
+
+const Career = async () => {
+    
+}
+
+export default Career;
